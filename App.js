5dc849bb1e9e07ebe1a4309057b427c8c968783d
mayur-vijayvargiya <126652481+mayur-vijayvargiya@users.noreply.github.com>
2025-03-31 20:31:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/Crud3.js';
+import Crud3 from './components/Crud3.js';
+
+function App() {
+  return (
+    <Crud3></Crud3>
+  );
+}
+
+export default App;
