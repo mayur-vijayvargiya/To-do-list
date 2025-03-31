1dd8a8a99f660db0378a6c2c7f8faea0b7456095
mayur-vijayvargiya <126652481+mayur-vijayvargiya@users.noreply.github.com>
2025-03-31 20:33:50 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import './components/Crud3.js';
-import Crud3 from './components/Crud3.js';
+import './Crud3.js';
+import Crud3 from './Crud3.js';
 
 function App() {
   return (
